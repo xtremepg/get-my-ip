ae8cd9664e9a3238cd968e7d6ce937390327240e
Rogerio Batista <rogerio.silva013@gmail.com>
2019-04-16 22:34:04 -0300

    AJuste de layout e exibicao do IP ao acessar a aplicacao

--- assets/js/get-ip.js
+++ assets/js/get-ip.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
+
+    obterIp(obterUrl('v4'));
+
     $('.btn-ip').click(function() {
-
         var version = $(this).data('version');
-        var url = version === 'v4' ? 'https://api.ipify.org/?format=json' : 'https://api6.ipify.org/?format=json';
-
-        $.get(url, function(response) {
-            $('#ip-result').html('<h2>Your Ip Address is: <strong>' + response.ip + '</strong></h2>').collapse();
-        });
+        var url = obterUrl(version);
+        obterIp(url);
     });
 });
+
+function obterUrl(version) {
+    return version === 'v4' ? 'https://api.ipify.org/?format=json' : 'https://api6.ipify.org/?format=json';
+}
+
+function obterIp(url) {
+    $.get(url, function(response) {
+        $('#ip-result').html('<h2>Your Ip Address is: <strong>' + response.ip + '</strong></h2>').collapse();
+    });
+}
